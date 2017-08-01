d34a4c80cc6be886a372db304025eb3fc14bd5da
michaelmcfadden <l00105787@student.lyit.ie>
2017-08-01 23:09:12 +0100

    michael test commit and push

--- src/main/java/com/assignment/mushroom/App.java
+++ src/main/java/com/assignment/mushroom/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello!" );
         System.out.println( "hi!" );
+        System.out.println( "Michael!" );
 
 
     }
