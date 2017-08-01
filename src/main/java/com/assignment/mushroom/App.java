15d2affd94ca5270acc277250716f299373b80a3
michaelmcfadden <l00105787@student.lyit.ie>
2017-08-01 23:27:04 +0100

    michael commit

--- src/main/java/com/assignment/mushroom/App.java
+++ src/main/java/com/assignment/mushroom/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello!" );
         System.out.println( "hi!" );
-        System.out.println( "Michael!" );
+        System.out.println( "Michael!!!!!" );
 
 
     }
